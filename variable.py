6fd400b06d682ef89d6fe9a919df6a12b3cbb63a
Konstantin34-P <tatyanash8908@gmail.com>
2024-11-09 18:53:01 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,12 @@
+# Вариант 1
+x = 12 # Количество выполненных ДЗ
+y = 1.5 # Количество затраченных часов
+p = 'Python' # Название курса
+print('Python, '+'всего задач: '+str(x), 'затрачено часов: '+str(y)+', среднее время выполнения: '+str(y/x)+' часа.')
+
+# Вариант 2
+Number_of_completed_homework_assignments = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = str(Number_of_hours_spent / Number_of_completed_homework_assignments)
+print(Course_name+', всего задач: '+str(Number_of_completed_homework_assignments), 'затрачено часов: '+str(Number_of_hours_spent)+', среднее время выполнения: '+str(Number_of_hours_spent/Number_of_completed_homework_assignments)+' часа.')
